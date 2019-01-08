19adfe8f59d6dadd8853b3ca1786d1d64b204978
crcortesd <cr.cortes.a@gmail.com>
2019-01-08 09:12:14 -0300

    Completed the module

--- src/app.js
+++ src/app.js
@@ -4,7 +4,9 @@ const express=require('express');
 const {accounts,users,writeJSON}=require('./data')
 
 const accountRoutes=require('./routes/accounts.js');
-const servicesRoutes=require('./routes/services.js')
+const servicesRoutes=require('./routes/services.js');
+
+
 
 const app=express();
 
@@ -12,7 +14,10 @@ app.set('views',path.join(__dirname,'views'));
 app.set('view engine','ejs');
 
 app.use(express.static(path.join(__dirname,'public')));
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({extended:true}));
+
+app.use('/services',servicesRoutes);
+app.use('/account',accountRoutes);
 
 
 
